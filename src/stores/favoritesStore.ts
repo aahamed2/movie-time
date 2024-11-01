f7ba40821e088cbcea1879eca220ea79d2ca4cbc
Amjad Ahamed <amjad.ahamed2@gmail.com>
2024-11-01 14:24:17 +0530

    chore: `persist` used to retain faves

--- src/stores/favoritesStore.ts
+++ src/stores/favoritesStore.ts
@@ -1,23 +1,28 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { TMovie } from '../types/movieTypes';
 
 export type TFavoriteStore = {
   favorites: TMovie[];
   toggleFavorite: (movie: TMovie) => void;
 };
-
-const useFavoritesStore = create<TFavoriteStore>((set) => ({
-  favorites: [],
-  toggleFavorite: (movie: TMovie) => {
-    set((state) => {
-      const isFavorite = state.favorites.some((fav) => fav.id === movie.id);
-      return {
-        favorites: isFavorite
-          ? state.favorites.filter((fav) => fav.id !== movie.id)
-          : [...state.favorites, movie],
-      };
-    });
-  },
-}));
+const useFavoritesStore = create<TFavoriteStore>()(
+  persist(
+    (set) => ({
+      favorites: [],
+      toggleFavorite: (movie: TMovie) => {
+        set((state) => {
+          const isFavorite = state.favorites.some((fav) => fav.id === movie.id);
+          return {
+            favorites: isFavorite
+              ? state.favorites.filter((fav) => fav.id !== movie.id)
+              : [...state.favorites, movie],
+          };
+        });
+      },
+    }),
+    { name: 'favorites-storage' }
+  )
+);
 
 export default useFavoritesStore;
