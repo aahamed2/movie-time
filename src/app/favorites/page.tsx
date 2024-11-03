52a2058fcb70a50f8522faefd1f4dee6882c2ada
Amjad Ahamed <amjad.ahamed2@gmail.com>
2024-11-03 13:25:21 +0530

    chore: favorites empty page cleanup

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -8,8 +8,7 @@ import styles from './Favorites.module.scss';
 
 export default function Favorites() {
   const [loading, setLoading] = useState<boolean>(true);
-  const favorites =
-    (useFavoritesStore((state) => state.favorites) as TMovie[]) || [];
+  const favorites = useFavoritesStore((state) => state.favorites) as TMovie[];
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -20,17 +19,19 @@ export default function Favorites() {
   }, []);
 
   if (loading) {
-    return <h2>Loading faves...</h2>;
-  }
-
-  if (favorites.length === 0) {
-    return <h2>No favorite movies found!</h2>;
+    return <h2>Loading favorites...</h2>;
   }
 
   return (
     <div className={styles.favorites}>
       <h1>My Favorite Movies</h1>
-      <MoviesList movies={favorites} />
+      {favorites.length > 0 ? (
+        <MoviesList movies={favorites} />
+      ) : (
+        <div className={styles.noFavorites}>
+          No favorite movies found! Browse the site to pick your favorites!
+        </div>
+      )}
     </div>
   );
 }
