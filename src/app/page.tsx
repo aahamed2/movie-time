bdf08b4ae0e1eddeb1ca8419cb68ec6836d708f2
Amjad Ahamed <amjad.ahamed2@gmail.com>
2024-11-04 09:16:08 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
+import React from 'react';
 import { fetchMovies } from '../utils/fetch';
 import MoviesList from '../components/movieList/MovieList';
 import styles from './page.module.scss';
-export default async function MoviesPage() {
-  const movies = await fetchMovies();
+import { TMovie } from '../types/movieTypes';
+
+const MoviesPage: React.FC = async () => {
+  const movies: TMovie[] = await fetchMovies();
 
   return (
     <div className={styles.homePageWrapper}>
       <div className={styles.homePageSnippet}>
         <div className={styles.textOverlay}>
           <h1>Welcome to My Movies</h1>
-          <p>"Browse through the world of movies! </p>
+          <p>Browse through the world of movies!</p>
         </div>
       </div>
       <MoviesList movies={movies} />
     </div>
   );
-}
+};
+
+export default MoviesPage;
